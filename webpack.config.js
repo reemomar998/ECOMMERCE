ae4a0525de4c8c6f9ff4d25ef027ee5a1c726d09
reem alsahouri <79793799+reemomar998@users.noreply.github.com>
2021-05-02 16:17:05 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,130 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin= require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+ 
+  entry:  {
+    app:'./src/index.js'
+  },
+  
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: '',
+    filename: "main.js"
+  },
+
+  mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    port: 1233,
+    overlay: true,
+    writeToDisk: true,
+  
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: require.resolve('jquery'),
+        loader: 'expose-loader',
+        options: {
+          exposes: ['$', 'jQuery'],
+        }
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+              {
+                loader: MiniCssExtractPlugin.loader, 
+                options: {
+                  publicPath: '../' 
+                }
+              },
+              'css-loader',
+              'sass-loader'
+            ]
+      },
+      
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+              esModule: false,
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+         
+          }
+        ]
+      }
+    ]
+  },
+ 
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "search.html",
+      template: "./src/search.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "checkout.html",
+      template: "./src/checkout.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "product.html",
+      template: "./src/product.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "payment.html",
+      template: "./src/payment.html",
+    }),
+
+    new MiniCssExtractPlugin({filename: "css/style.css"}),
+
+    new OptimizeCSSAssetsPlugin({}),
+
+  ],
+
+};
